8a8ce067693d0be8ad774044d8cbe2b288e7dcb7
WKuipers <wietse@kuiprs.nl>
2014-06-04 20:02:45 +0200

    GEFIXT

---
+++ app/src/main/java/com/tippitytapv2/app/Game.java
@@ -0,0 +1,84 @@
+package com.tippitytapv2.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+
+public class Game extends Activity {
+
+    private MediaPlayer m;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AudioManager AM = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        AM.setStreamMute(AudioManager.STREAM_MUSIC, false);
+        setContentView(R.layout.activity_game);
+        Intent intent = getIntent();
+        String song_uri = intent.getStringExtra(MainMenu.SONG_URI);
+
+        Uri Song;
+        Song = Uri.parse(song_uri);
+        m = new MediaPlayer();
+        m.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            m.setDataSource(getApplicationContext(), Song);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            m.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        m.start();
+        TextView song_name_view = (TextView)findViewById(R.id.song_playing);
+        MediaMetadataRetriever meta = new MediaMetadataRetriever();
+        meta.setDataSource(getApplicationContext(),Song);
+        String song_name = meta.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        song_name_view.setText(song_name);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        m.release();
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.game, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/tippitytapv2/app/MainMenu.java
@@ -0,0 +1,42 @@
+package com.tippitytapv2.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.provider.MediaStore;
+
+import android.view.View;
+
+
+
+public class MainMenu extends Activity {
+
+    protected static final int REQUEST_SONG = 1;
+    protected static final String SONG_URI = "SONG_URI";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+
+
+
+    public void playButton (View view)        {
+        Intent selectSong = new Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(selectSong, REQUEST_SONG);
+    }
+
+    public void onActivityResult(int RequestCode, int resultCode, Intent intent){
+        if(RequestCode == REQUEST_SONG){
+            Intent toGame = new Intent(this, Game.class);
+            Uri song = intent.getData();
+            assert song != null;
+            toGame.putExtra(SONG_URI,song.toString());
+            startActivity(toGame);
+        }
+    }
+}
